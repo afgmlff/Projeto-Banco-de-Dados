23a1360338d1f8b7270417087a251764626bf24f
Leonardo Alves <alves.leonardo.t@gmail.com>
2019-06-29 16:31:50 -0300

    CRUD Inicial
    - CRUD apenas com Usuarios

---
+++ CRUD/src/main/MainMenu.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import model.DAO.UsuarioDAO;
+
+public class MainMenu extends JPanel implements ActionListener {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    protected JButton b1, b2, b3;
+    protected UsuarioDAO udao = new UsuarioDAO();
+
+    public MainMenu() {
+	b1 = new JButton("Cadastrar");
+	b1.setActionCommand("cadastro");
+	b2 = new JButton("Consultar");
+	b2.setActionCommand("consulta");
+	b3 = new JButton("Deletar");
+	b3.setActionCommand("delecao");
+
+	b1.addActionListener(this);
+	b2.addActionListener(this);
+	b3.addActionListener(this);
+
+	add(b1);
+	add(b2);
+	add(b3);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	if ("cadastro".equals(e.getActionCommand())) {
+	    udao.MenuInsere();
+	} else if ("consulta".equals(e.getActionCommand())) {
+	    udao.MenuConsulta();
+	} else if ("delecao".equals(e.getActionCommand())) {
+	    udao.MenuDeleta();
+	}
+    }
+
+    public static void criaJanela() {
+	JFrame frame = new JFrame("CRUD - Academia");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	MainMenu menu = new MainMenu();
+	menu.setOpaque(true);
+	frame.setContentPane(menu);
+
+	frame.pack();
+	frame.setVisible(true);
+    }
+}
---
+++ CRUD/src/model/DAO/MenuDAO.java
@@ -0,0 +1,48 @@
+package model.DAO;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+
+public class MenuDAO extends JPanel {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    protected JFileChooser jfc;
+    protected static String s = null;
+    protected final static JFrame frame = new JFrame("CRUD - Academia");
+
+    public MenuDAO() {
+	jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+	FileNameExtensionFilter filter = new FileNameExtensionFilter("Imagens jpg", "jpg", "jpeg");
+	jfc.setFileFilter(filter);
+	jfc.setDialogTitle("Selecione a foto do usuário");
+
+	int retorno = jfc.showOpenDialog(null);
+
+	if (retorno == JFileChooser.APPROVE_OPTION) {
+	    s = jfc.getSelectedFile().getAbsolutePath();
+	}
+    }
+
+    public static String criaJanela() {
+	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+	MenuDAO menu = new MenuDAO();
+	menu.setOpaque(true);
+	frame.setContentPane(menu);
+
+	frame.setVisible(true);
+	frame.pack();
+
+	if (s != null && s.length() > 0) {
+	    frame.setVisible(false);
+	    frame.dispose();
+	    return s;
+	}
+	return null;
+    }
+}
